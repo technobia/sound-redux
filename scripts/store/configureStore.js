58570f373ed1676660328241fc258cc34594a8f9
binh <binh.tran@8bitrockr.com>
2017-06-28 10:10:15 +0700

    modify config store to use redux devTools

--- scripts/store/configureStore.js
+++ scripts/store/configureStore.js
@@ -5,7 +5,11 @@ import rootReducer from '../reducers/index';
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
 
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = createStoreWithMiddleware(
+      rootReducer,
+      initialState,
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+  );
 
   return store;
 }
